f621477d139508c23e6cce7ac8d28e2433ead39f
gopileau <vanessagonzalez99@hotmail.com>
2025-06-12 01:28:48 +0200

    Mise à jour de Database.php et config.php

--- app/Models/Database.php
+++ app/Models/Database.php
@@ -2,21 +2,27 @@
 namespace App\Models;
 
 use PDO;
+use PDOException;
 
 class Database {
     protected $connection;
 
     public function __construct() {
-        $this->connection = new PDO(
-            'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME,
-            DB_USER,
-            DB_PASS
-        );
-        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        try {
+            $this->connection = new PDO(
+                'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME,
+                DB_USER,
+                DB_PASS
+            );
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Erreur de connexion à la base de données : " . $e->getMessage());
+        }
     }
 
     public function getConnection() {
         return $this->connection;
     }
 }
+
 
